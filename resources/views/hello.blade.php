914085aca85ff55c7a136283b266c7b371fc4bc3
homuryuzz <homuryuzz@example.com>
2026-07-12 04:40:44 +0900

    create hello page!

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>初期設定</title>
+</head>
+<body>
+    <h1>Laravelを使ってアプリを作っていく</h1>
+</body>
+</html>
